5cfa47a7ce099ff22b28f193a4df7ee3a7ab1d2c
darkwebtourguide <accounts@chrisadams.io>
2020-09-11 12:41:48 -0400

    code graveyard

---
+++ onice.js
@@ -0,0 +1,47 @@
+// //Audio Player Icon
+// _showDialogButton = document.getElementById("show-radio");
+
+// var dialog;
+// function showDialog() {
+//   console.log("Radio Currently Running");
+//   _showDialogButton.disabled = true;
+//   if (!dialog) {
+//     var id = "radio";
+//     // Instanciate the Dialog Box
+//     dialog = new DialogBox(id, callbackDialog);
+//   }
+//   // Show Dialog Box
+//   dialog.showDialog();
+
+//   // Receive result from Dialog Box
+//   function callbackDialog(btnName) {
+//     _showDialogButton.disabled = false;
+//     _showDialogButton.focus();
+//     if (btnName == "close") console.log("Radio Window Closed");
+//     else console.log(" Radio Window Closed");
+//   }
+// }
+
+// //Settings Icon
+// _showDialogButton = document.getElementById("show-settings");
+
+// var dialog;
+// function showInfo() {
+//   console.log("Settings Currently Running");
+//   _showDialogButton.disabled = true;
+//   if (!dialog) {
+//     var id = "settings";
+//     // Instanciate the Dialog Box
+//     dialog = new DialogBox(id, callbackDialog);
+//   }
+//   // Show Dialog Box
+//   dialog.showInfo();
+
+//   // Receive result from Dialog Box
+//   function callbackDialog(btnName) {
+//     _showDialogButton.disabled = false;
+//     _showDialogButton.focus();
+//     if (btnName == "close") console.log("Settings Window Closed");
+//     else console.log(" Settings Window Closed");
+//   }
+// }
