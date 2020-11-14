eaa844302e157b6eac475000a868c02b8bd0656e
darkwebtourguide <accounts@chrisadams.io>
2020-11-14 15:47:13 -0500

    set up express router

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+//beatmango home page
+router.get('/', function (req, res) {res.render('pages/index')})
+
+//lyri home page
+router.get('/info', function (req, res) {res.render('pages/lyri')})
+
+module.exports = router
